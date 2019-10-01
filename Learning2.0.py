2c6bf345a059f187fb153bcaa333ca2372b3b152
IllianTear <condorvirus@gmail.com>
2019-10-01 15:37:55 -0500

    playing with print

--- Learning2.0.py
+++ Learning2.0.py
@@ -21,3 +21,6 @@ print(3 + 2 < 5 - 7)
 # Is the area of a 5x6 object larger than the area of a 4x7 object?
 print("Is 5 * 6 > 4 * 7 true?")
 print(5 * 6 > 4 * 7)
+# A floating number determines how many decimals are used in a number
+print("%.2f" % 30)
+# This would be thirty using floating numbers
